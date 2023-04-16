5953ea42d0252caff3071aa069e9963da36ddd9a
victoriabogetti <victoriabogetti>
2023-04-15 21:42:56 -0300

    cambioooooooooos

--- public/js/validacionLogin.js
+++ public/js/validacionLogin.js
@@ -1,33 +1,35 @@
-window.addEventListener("load", function(){
-    /*ACA NOS TRAEMOS CADA UNO DE LOS ITEMS DEL FORMULARIO*/
-    let formulario = document.querySelector("form.loginform")
-    let emailLogin = document.querySelector("#emailLogin")
-    let passLogin = document.querySelector("#passLogin")
-    /*let db = require("../database/models")*/
+window.addEventListener("load", function () {
+  /*ACA NOS TRAEMOS CADA UNO DE LOS ITEMS DEL FORMULARIO*/
+  let formulario = document.querySelector("form.loginform")
+  let emailLogin = document.querySelector("#emailLogin")
+  let passLogin = document.querySelector("#passLogin")
+  /*let db = require("../database/models")*/
 
-    /*ESTA ES LA VALIDACION DEL REGISTRO*/
-  formulario.addEventListener("submit", function(a){
+  /*ESTA ES LA VALIDACION DEL REGISTRO*/
+  
+  let ulErrorLogin = document.querySelector("div.errorLogin ul");
 
-        let errorLogin = [];
-       
 
-        if (emailLogin.value == "") {
-            errorLogin.push("Debe elegir un email")
-        }
+  formulario.addEventListener("submit", function (a) {
+   /* errorLogin = errorLogin.splice(0, array.length)*/
+      let errorLogin = [];
+      if (emailLogin.value == "") {
+        errorLogin.push("Debe elegir un email")
+      }
 
-        
-       if (passLogin.value == ""){
+
+      if (passLogin.value == "") {
         errorLogin.push("Debe poner una contraseña")
-        }
+      }
 
-        if (errorLogin.length>0) {
-            a.preventDefault();
-            
-          }
-          let ulErrorLogin = document.querySelector("div.errorLogin ul");
-            for (let i = 0; i < errorLogin.length; i ++){
-              ulErrorLogin.innerHTML += "<li>" + errorLogin[i] + "</li>"
-            }
+      if (errorLogin.length > 0) {
+        a.preventDefault();
+
+      ulErrorLogin.innerHTML = "";
+      
+      for (let i = 0; i < errorLogin.length; i++) {
+        ulErrorLogin.innerHTML += "<li>" + errorLogin[i] + "</li>"
+      }}
 
 
     })})
